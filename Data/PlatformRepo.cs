05ff879a06e555e37bdb8b231143d515e0abc7aa
MARQUEZ Jesu Amet HP <c.jmarquez@ternium.com>
2021-10-10 10:08:50 -0300

    AppDbContext, PlatformRepo, SeedData

---
+++ Data/PlatformRepo.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            _context.Platforms.Add(platform);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform GetById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() >= 0; 
+        }
+    }
+}
