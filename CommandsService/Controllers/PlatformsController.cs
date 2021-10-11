b6b6fd237bafd2ba6e7c6fc62a878eb918474ad5
MARQUEZ Jesu Amet HP <c.jmarquez@ternium.com>
2021-10-11 13:52:40 -0300

    CommandsService

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+            return Ok("Inbound test from Platform Controller");
+        }
+    }
+}
